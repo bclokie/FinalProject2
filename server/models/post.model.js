95d956833bef1ad98211a5d0e12f635dc259cb7b
Braden <braden.clokie@gmail.com>
2023-03-22 21:44:30 -0400

    Added post models

---
+++ server/models/post.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  title: String,
+  plantName: String,
+  image: String,
+  author: String,
+  description: String,
+  latitude: String,
+  longitude: String
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
